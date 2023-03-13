fbed61345dd03a8c9075868e53fd047d0f876a43
Vitor Orsini <vitorluigiorsini@gmail.com>
2023-03-13 10:17:54 -0300

    feat: add auto demand on events table

--- lib/eventsDdb-stack.ts
+++ lib/eventsDdb-stack.ts
@@ -20,28 +20,29 @@ export class EventsDdbStack extends cdk.Stack {
         type: dynamodb.AttributeType.STRING,
       },
       timeToLiveAttribute: 'ttl',
-      billingMode: dynamodb.BillingMode.PROVISIONED,
-      readCapacity: 1,
-      writeCapacity: 1,
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      // readCapacity: 1,
+      // writeCapacity: 1,
     });
-    const readScale = this.table.autoScaleReadCapacity({
-      maxCapacity: 2,
-      minCapacity: 1,
-    });
-    readScale.scaleOnUtilization({
-      targetUtilizationPercent: 50,
-      scaleInCooldown: cdk.Duration.seconds(60),
-      scaleOutCooldown: cdk.Duration.seconds(60),
-    });
+    // Modo de configuração Auto Scaling para faturamento Provisionado
+    // const readScale = this.table.autoScaleReadCapacity({
+    //   maxCapacity: 2,
+    //   minCapacity: 1,
+    // });
+    // readScale.scaleOnUtilization({
+    //   targetUtilizationPercent: 50,
+    //   scaleInCooldown: cdk.Duration.seconds(60),
+    //   scaleOutCooldown: cdk.Duration.seconds(60),
+    // });
 
-    const writeScale = this.table.autoScaleWriteCapacity({
-      maxCapacity: 4,
-      minCapacity: 1,
-    });
-    writeScale.scaleOnUtilization({
-      targetUtilizationPercent: 30,
-      scaleInCooldown: cdk.Duration.seconds(60),
-      scaleOutCooldown: cdk.Duration.seconds(60),
-    });
+    // const writeScale = this.table.autoScaleWriteCapacity({
+    //   maxCapacity: 4,
+    //   minCapacity: 1,
+    // });
+    // writeScale.scaleOnUtilization({
+    //   targetUtilizationPercent: 30,
+    //   scaleInCooldown: cdk.Duration.seconds(60),
+    //   scaleOutCooldown: cdk.Duration.seconds(60),
+    // });
   }
 }
